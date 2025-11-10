9cf1f30cd26eeb9668f326e28d0501ad0f22fd36
ilainDAM <jacoboperdon@gmail.com>
2025-11-10 16:52:03 +0100

    Martes

--- src/main/java/com/example/proyectodisenioima/LoginController.java
+++ src/main/java/com/example/proyectodisenioima/LoginController.java
@@ -1,14 +1,30 @@
 package com.example.proyectodisenioima;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
+    @FXML private TextField email;
+    @FXML private PasswordField contrasenia;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLogin(ActionEvent event) throws FileNotFoundException {
+        Scanner leer = new Scanner(new File("usuarios.txt"));
+        String[] lineaSeparada;
+        String linea;
+        while (leer.hasNext()){
+            linea = leer.nextLine();
+            lineaSeparada = linea.split(",");
+            if (email.getText().equals(lineaSeparada[0])&&contrasenia.getText().equals(lineaSeparada[1])){
+                System.out.println("estas dentro");
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.proyectodisenioima {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
 
     opens com.example.proyectodisenioima to javafx.fxml;
