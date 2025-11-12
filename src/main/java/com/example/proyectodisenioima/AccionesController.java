a806e14f5bc6cf54b13c9d1c8aa1e255cc31f22f
ilain <monicaloki02@gmail.com>
2025-11-13 00:51:00 +0100

    Entrega prototipo

---
+++ src/main/java/com/example/proyectodisenioima/AccionesController.java
@@ -0,0 +1,44 @@
+package com.example.proyectodisenioima;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AccionesController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Controlador de Acciones inicializado");
+    }
+    
+    @FXML
+    protected void abrirLogin(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1920, 1080);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al cargar login: " + e.getMessage());
+        }
+    }
+    
+    @FXML
+    protected void cerrarVentana(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+}
--- src/main/java/com/example/proyectodisenioima/LoginApplication.java
+++ src/main/java/com/example/proyectodisenioima/LoginApplication.java
@@ -3,6 +3,7 @@ package com.example.proyectodisenioima;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,9 @@ import java.io.IOException;
 public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("/font/Jersey15-Regular.ttf"), 14);
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
