27ba934a5fdd82393f59a5d7fc10a257ce59e4c1
ilain <jacoboperdon@gmail.com>
2025-11-27 18:09:35 +0000

    Terminado la personalizacion de temas

--- src/main/java/com/example/proyectodisenioima/AjustesController.java
+++ src/main/java/com/example/proyectodisenioima/AjustesController.java
@@ -1,5 +1,6 @@
 package com.example.proyectodisenioima;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
@@ -15,7 +16,7 @@ public class AjustesController {
     private VBox nodo;
     @FXML
     public void onOriginal() throws IOException {
-        this.crearCss("#FFEECF","#FFC482","#65DEF1","#2B59C3","#04F06A","black","white","black");
+        this.crearCss("#FFEECF","#FFC482","#65DEF1","#2B59C3","#04F06A","black","white");
     }
 
     @FXML
@@ -29,31 +30,81 @@ public class AjustesController {
     }
 
     @FXML
-    public void onPersonalizado(){
-
+    public void onPersonalizado(ActionEvent event){
+        new Utilidades().abrirVentana(event,"colorPersonalizado.fxml","Color personalizado");
     }
+
     public void crearCss(String pbg,
-                                String sbg,
-                                String prs,
-                                String srs,
-                                String trs,
-                                String pff,
-                                String sff,
-                                String sp) throws IOException {
+                         String sbg,
+                         String prs,
+                         String srs,
+                         String trs,
+                         String pff,
+                         String sff) throws IOException {
         FileWriter fw = new FileWriter("src/main/resources/CSS/root.css");
         BufferedWriter escribir = new BufferedWriter(fw);
-        String css=" @font-face {\n" +
+        String css = " @font-face {\n" +
                 "            font-family: \"Jersey 15\";\n" +
                 "            src: url(\"../font/Jersey15-Regular.ttf\") format(\"truetype\");\n" +
                 "        }\n" +
                 "*{\n" +
-                "            -fx-principal-bg-color:"+pbg+";\n" +
-                "            -fx-secundario-bg-color:"+sbg+";\n" +
-                "            -fx-principal-rs-color:"+prs+";\n" +
-                "            -fx-secundario-rs-color:"+srs+";\n" +
-                "            -fx-terciario-rs-color:"+trs+";\n" +
-                "            -fx-principal-font-color:"+pff+";\n" +
-                "            -fx-secundario-font-color:"+sff+";\n" +
+                "            -fx-principal-bg-color:" + pbg + ";\n" +
+                "            -fx-secundario-bg-color:" + sbg + ";\n" +
+                "            -fx-principal-rs-color:" + prs + ";\n" +
+                "            -fx-secundario-rs-color:" + srs + ";\n" +
+                "            -fx-terciario-rs-color:" + trs + ";\n" +
+                "            -fx-principal-font-color:" + pff + ";\n" +
+                "            -fx-secundario-font-color:" + sff + ";\n" +
+                "            -fx-font-family: 'Jersey 15';\n" +
+                "            -fx-especial:black;\n" +
+                "        }\n" +
+                "\n" +
+                ".fuente32{\n" +
+                "            -fx-font-size: 32px;\n" +
+                "        }\n" +
+                ".fuente42{\n" +
+                "            -fx-font-size: 42px;\n" +
+                "        }\n" +
+                ".fuente62{\n" +
+                "            -fx-font-size: 62px;\n" +
+                "        }.menuSuperior{\n" +
+                "            -fx-background-color: -fx-secundario-bg-color;\n" +
+                "            -fx-border-color: black;\n" +
+                "            -fx-border-width: 4px;\n" +
+                "        }\n" +
+                " .tituloApartado{\n" +
+                "            -fx-background-color: -fx-principal-rs-color;\n" +
+                "            -fx-border-color: black;\n" +
+                "            -fx-border-width: 0 4px 4px 4px;\n" +
+                "        }";
+
+        escribir.write(css);
+        escribir.close();
+        this.resetearCSS();
+    }
+
+    public void crearCss(String pbg,
+                         String sbg,
+                         String prs,
+                         String srs,
+                         String trs,
+                         String pff,
+                         String sff,
+                         String sp) throws IOException {
+        FileWriter fw = new FileWriter("src/main/resources/CSS/root.css");
+        BufferedWriter escribir = new BufferedWriter(fw);
+        String css = " @font-face {\n" +
+                "            font-family: \"Jersey 15\";\n" +
+                "            src: url(\"../font/Jersey15-Regular.ttf\") format(\"truetype\");\n" +
+                "        }\n" +
+                "*{\n" +
+                "            -fx-principal-bg-color:" + pbg + ";\n" +
+                "            -fx-secundario-bg-color:" + sbg + ";\n" +
+                "            -fx-principal-rs-color:" + prs + ";\n" +
+                "            -fx-secundario-rs-color:" + srs + ";\n" +
+                "            -fx-terciario-rs-color:" + trs + ";\n" +
+                "            -fx-principal-font-color:" + pff + ";\n" +
+                "            -fx-secundario-font-color:" + sff + ";\n" +
                 "            -fx-font-family: 'Jersey 15';\n" +
                 "            -fx-especial:"+sp+";\n" +
                 "        }\n" +
--- src/main/java/com/example/proyectodisenioima/ColorPersonalizadoController.java
+++ src/main/java/com/example/proyectodisenioima/ColorPersonalizadoController.java
@@ -1,7 +1,43 @@
 package com.example.proyectodisenioima;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+
 public class ColorPersonalizadoController {
+    @FXML
+    ColorPicker uno;
+    @FXML
+    ColorPicker dos;
+    @FXML
+    ColorPicker tres;
+    @FXML
+    ColorPicker cuatro;
+    @FXML
+    ColorPicker cinco;
+    @FXML
+    ColorPicker seis;
+    @FXML
+    ColorPicker siete;
 
 
+
+    private String toHexColor(Color c) {
+        return c.toString().replace("0x", "#").substring(0, 7);
+    }
+
+    public void onBotonEnviar() throws IOException {
+        new AjustesController().crearCss(
+                toHexColor(uno.getValue()),
+                toHexColor(dos.getValue()),
+                toHexColor(tres.getValue()),
+                toHexColor(cuatro.getValue()),
+                toHexColor(cinco.getValue()),
+                toHexColor(seis.getValue()),
+                toHexColor(siete.getValue())
+        );
+    }
 
 }
